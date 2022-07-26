3dfece9e7e2ac08b1e6c816c8458f4b8e0a568ca
Natalie Marques <namqsf@gmail.com>
2022-07-26 20:10:30 -0300

    Add files via upload

---
+++ Exerc1.py
@@ -0,0 +1,59 @@
+# Estudos de Python básico sobre strings e ints :)
+# 1 frase: Eu amo o João Lucas e somos um do outro há 1 ano, desde que a shark nos acolheu no nosso primeiro 25
+
+'''emissor = 'eu'
+quem = 'João Lucas'
+tempo = 1
+quem2 = 'shark'
+colocação = 'primeiro'
+dia = 25
+
+print (emissor  + ' amo o ' + quem + ' e somos um do outro há ' + str(tempo) + 'ano, desde que a ' + quem2 + ' nos acolheu no nosso ' + colocação + ' ' + str( dia)) '''
+
+# 2 frase: 2024 é o ano que eu terminarei a faculdade de ADS, após 6 semestres mais um TCC com bem mais de 30 páginas
+'''
+year = 2024
+year = str (year)
+quem = 'eu'
+terminaroq= 'faculdade'
+curso = 'ADS'
+ndeSem = '6'
+umOq = 'TCC'
+nPaginas = 30
+nPaginas = str (nPaginas)
+
+print (year + ' é o ano que ' + quem + ' terminarei a ' + terminaroq + ' de ' + terminaroq +  ', após ' + ndeSem + ' semestres mais um ' + umOq + ' com bem mais de ' + nPaginas + ' páginas.') '''
+
+# 3 frase: João tem 1 cachorro chamado Zé, ele passeia 3 vezes por dia e faz 1 coco a cada passeio.
+'''
+nome = 'João'
+qnt = '1'
+nome2 = 'Zé'
+qnt2 = '3'
+
+print ( nome + ' tem ' + qnt + ' cachorro chamado ' + nome2 + ', ele passeia ' + qnt2 + ' vezes por dia e faz ' + qnt + ' coco a cada passeio.' )
+'''
+# 4 frase: Todo dia dou 5 reais aos meus 2 filhos, Camilo e Tryndashe, para comerem 1 salgado.
+'''
+qnt = 5
+qnt = str (qnt)
+qnt2 = 2
+qnt2 = str (qnt2)
+nome = 'Camilo'
+nome2 = 'Tryndashe'
+qnt3 = 1
+qnt3 = str (qnt3)
+comida = 'salgado'
+
+print ('Todo dia dou ' + qnt + ' reais aos meus ' + qnt2 + ' filhos, ' + nome + ' e ' + nome2 + ', para comerem ' + qnt3 + ' ' + comida + '.')
+'''
+
+# 5 frase: Sempre levo meus 2 cachorros, Mel e Jimmy, para passear 3 vezes por dia. 
+'''
+qnt = 2
+nome = 'Mel'
+nome2 = 'Jimmy'
+qnt2 = 3
+
+print ('Sempre levo meus ' + str(qnt) + ' cahorros, ' + nome + ' e ' + nome2 + ', para passear ' + str(qnt2) + ' vezes por dia.')
+'''

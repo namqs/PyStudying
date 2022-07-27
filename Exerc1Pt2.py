65da3dd9e9b1b912f5a4cf5621f4892b77529d0f
Natalie Marques <namqsf@gmail.com>
2022-07-27 12:45:16 -0300

    Create Exerc1Pt2.py
    Parte 2 dos exercícios básicos de strings e intergers

---
+++ Exerc1Pt2.py
@@ -0,0 +1,56 @@
+# 6º frase: Quando João ia para a escola, pegava 2 ônibus na ida, e na volta 1 metrô
+
+nome = 'João'
+lugar = 'escola'
+qnt = '2'
+transp = 'ônibus'
+qnt2 = '1'
+transp2 = 'metrô'
+
+print ('Quando ' + nome + ' ia para a ' + lugar + ', pegava ' + qnt + ' ' + transp + ' na ida, e na volta ' + qnt2 + ' ' + transp2 + '.')
+'''
+
+# 7 frase: Na casa de João moram 5 pessoas, e quando tem bolo, suas 2 irmãs comem tudo
+
+'''
+lugar = 'casa'
+nome = 'João'
+qnt = '5'
+comida = 'bolo'
+qnt2 = '2'
+quem = 'irmãs'
+
+print ('Na ' + lugar + ' de ' + nome + ' moram ' + qnt + ' pessoas, e quando tem ' + comida + ', suas ' + qnt2 + ' ' + quem + ' comem tudo.')
+'''
+
+# 8 frase: Ele ama a Natalie, eles tem 1 constelação juntos e ela é composta por 13 estrelas no céu.
+
+'''
+nome = 'ele'
+nome2 = 'Natalie'
+qnt = '1'
+objeto = 'constelação'
+qnt2 = '13'
+local = 'céu'
+
+print ( nome + ' ama a ' + nome2 + ', eles tem ' + qnt + ' ' + objeto + ' juntos e ela é composta por ' + qnt2 + ' estrelas no ' + local + '.')
+'''
+'''
+# 9º frase: Agosto é o melhor mês para Natalie, possui 31 dias e o aniversário dela é no dia 30.
+
+mês = 'Agosto'
+nome = 'Natalie'
+qnt = 31
+qnt2 = 30
+
+print ( mês + ' é o melhor mês para  ' + nome + ', possui ' + str (qnt) + ' dias e o aniversário dela é no dia ' + str (qnt2) + '.')
+'''
+'''
+# 10º frase: Python é uma ótima linguagem, a número 1.
+
+ling = 'Python'
+adj = 'ótima'
+qnt = 1
+
+print ( ling + ' é uma ' + adj + ' linguagem, a número ' + str (qnt) + '.')
+'''

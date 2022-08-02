34f7200ea050377501ee8dffc97626b1576dbb86
Natalie Marques <namqsf@gmail.com>
2022-08-01 21:34:42 -0300

    Add files via upload

---
+++ MetodoStrings.py
@@ -0,0 +1,7 @@
+mensagem = ('Eu quero ir para a lua')
+
+print (mensagem.lower()) #tudo minusculo
+print (mensagem.upper()) #tudo maiusculo
+print (mensagem.find('i')) #mostra o index
+print (mensagem.replace('a', 'e')) #substitui
+print (mensagem.strip()) #remove todos os espaços antes do primeiro caracter
